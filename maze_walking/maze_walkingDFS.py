21d69e0814b83c3a6646a18f021c2d0292c47b70
Hsien-Hsin Sean Lee <leehs@fb.com>
2021-02-03 17:57:12 -0500

    Update DFS version

--- maze_walking/maze_walkingDFS.py
+++ maze_walking/maze_walkingDFS.py
@@ -99,66 +99,23 @@ def dfs(board, x, y, ex, ey, path):
         return False
 
 
-def bfs(board, sx, sy, ex, ey):
-    if (board[sx][sy]==1):
-        return False
-
-    q = [(sx, sy)]
-    while len(q) > 0:
-        #print("len(q) =", len(q))
-        pos = q[0]
-        #print("q[0] == ", pos)
-        del q[0]
-
-        # Reach the end
-        if pos[0] == ex and pos[1] == ey:
-            return True
-        next_pos = [(x, y) for x, y in [
-            (pos[0] + 1, pos[1]),
-            (pos[0] - 1, pos[1]),
-            (pos[0], pos[1] + 1),
-            (pos[0], pos[1] - 1)
-            ] if x >= 0 and x < len(board) and y >=0 and y < len(board[0]) and
-                board[x][y] == 0]
-
-        print("next_pos", next_pos)
-        for n in next_pos:
-            #print("n in next_pos",n[0], n[1], "added POS", pos)
-            board[n[0]][n[1]] = pos
-            q.append(n)
-    return False
-
-
 
 # Start of the Program
 start_x = 0
-start_y = 0
-end_x = len(maze) - 1
-end_y = len(maze[0]) - 1
+start_y = 3
+end_x = 0 #len(maze) - 1
+end_y = 0 #len(maze[0]) - 1
+
+print(maze[0])
+print(maze[1])
+print(maze[2])
 
 
 # DFS solution
-"""path = []
-route = dfs(mazeDFS, start_x, start_y, end_x, end_y, path)
+
+path = []
+route = dfs(maze, start_x, start_y, end_x, end_y, path)
 if (route):
     print("Depth-First Search--> path is", path)
 else:
     print("Depth-First Search--> no path")
-"""
-
-# BFS solution
-path = []
-found = bfs(maze, start_x , start_y, end_x, end_y)
-if (found):
-    #print(len(maze), len(maze[0]), maze[1][2])
-    #print("updated maze", maze)
-    pos = (maze[len(maze)-1][len(maze[0])-1]) # backtrack from the exit position
-    # start from the destination
-    path.insert(0, (end_x, end_y))
-    while not (pos[0] == start_x and pos[1] == start_y):
-        path.insert(0, pos)
-        pos = maze[pos[0]][pos[1]]
-    path.insert(0, pos) # (start_x, start_y))
-    print("Breadth-First Search--> path is", path)
-else:
-    print("Breadth-First Search--> no path")
