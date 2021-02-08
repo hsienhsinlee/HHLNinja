754fbb7bae6556e471b5ca8f11f1b7d21a42b803
Hsien-Hsin Sean Lee <leehs@fb.com>
2021-02-08 11:10:01 -0800

    new palindrome_check

---
+++ palindrome_check/palindrome_check.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+"""
+*** Description ***
+
+Given a string, we want to check if that string can form a palindromes.
+Return a bool indicating true or false
+
++ Positive Examples: aba, abba, kayak, rotator
+- Negative Examples: acb, aabc
+
+"""
